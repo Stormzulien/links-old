176c5938d236202a8abcdbe1d4b7b4dc37a50791
Stormzulien <jayvadgama2009@gmail.com>
2026-02-17 21:54:17 +0000

    Add files via upload

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -5,8 +5,22 @@ import globalConfig from "https://stormzulien.github.io/data/global_config.js";
 
 // const redirect = false;
 const redirect = globalConfig.linksOldRedirect;
-  
+
 if (redirect) {
+  // document.querySelector("body").innerHTML += `
+  //   <div id="redirect-overlay">
+  //     <span>Redirecting to newer page...</span>
+  //   </div>
+  // `;
+
+  // const redirectOverlay = document.querySelector("#redirect-overlay");
+  // redirectOverlay.classList.add("hidden");
+  
   const pn = window.location.pathname.replace("/links-old/", "");
+  console.warn(`Redirecting to: https://stormzulien.github.io/links/${pn}`);
   window.location.replace(`https://stormzulien.github.io/links/${pn}`);
+  
+} else {
+  const redirectOverlay = document.querySelector("#redirect-overlay");
+  redirectOverlay.classList.add("hidden");
 }
